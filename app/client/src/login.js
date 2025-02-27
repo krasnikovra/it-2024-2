78a8dcaaf68873f560a4ea51655405f33fc420d0
krasnikovra <roma_krasnikov02@mail.ru>
2025-02-27 21:14:33 +0300

    example of static lang switch for login page

--- app/client/src/login.js
+++ app/client/src/login.js
@@ -1,10 +1,17 @@
 import { mount, el } from '../node_modules/redom/dist/redom.es';
+import { library } from './utils/library';
 import LoginForm from './widget/loginForm'
 
-const lang = 'ru'; // 'ru', 'en'
+const lang = 'en'; // 'ru', 'en'
 
 class LoginPage {
-    constructor() {
+    constructor(settings = {}) {
+        const {
+            lang = 'ru',
+        } = settings;
+
+        this._lang = lang;
+
         this.el = this._ui_render();
     }
 
@@ -12,15 +19,28 @@ class LoginPage {
         return (
             <div className='container-md'>
                 <div className='mb-3'>
-                    <h1 className='text-center'>Вход</h1>
+                    <h1 this="_ui_h1" className='text-center'>Вход</h1>
                 </div>
-                <LoginForm />
+                <LoginForm this="_ui_login_form" />
             </div>
         );
+    }
+
+    update = () => {
+        this._ui_h1.textContent = library[this._lang]['login'];
+        
+        const data = {
+            lang: this._lang
+        };
+        this._ui_login_form.update(data);
+    }
+
+    onmount = () => {
+        this.update();
     }
 }
 
 mount(
     document.getElementById("main"),
-    <LoginPage />
+    <LoginPage lang={lang} />
 );
--- app/client/src/widget/loginForm.js
+++ app/client/src/widget/loginForm.js
@@ -1,6 +1,7 @@
 import { mount, el } from '../../node_modules/redom/dist/redom.es';
 import Button from '../atom/button';
 import Link from '../atom/link';
+import { library } from '../utils/library';
 import LoginAndPassForm from './loginAndPassForm';
 
 export default class LoginForm {
@@ -12,13 +13,31 @@ export default class LoginForm {
         return (
             <div>
                 <div className='mb-4'>
-                    <LoginAndPassForm />
-                    <p><small>Нет аккаунта? <Link text='Зарегистрироваться' href='./register.html'/></small></p>
+                    <LoginAndPassForm this='_ui_login_and_pass_form' />
+                    <p>
+                        <small>
+                            <span this='_ui_span'>Нет аккаунта?</span>&nbsp;
+                            <Link this='_ui_link' text='Зарегистрироваться' href='./register.html' />
+                        </small>
+                    </p>
                 </div>
                 <div className='text-center'>
-                    <Button text='Войти' className='w-100' type='primary' />
+                    <Button this='_ui_button' text='Войти' className='w-100' type='primary' />
                 </div>
             </div>
         );
+    }
+
+    update = (data) => {
+        const { lang } = data;
+
+        this._ui_login_and_pass_form.update(data);
+        this._ui_link.update({
+            text: library[lang]['to_register'],
+        });
+        this._ui_span.textContent = library[lang]['no_account_question'];
+        this._ui_button.update({
+            text: library[lang]['to_login']
+        });
     }
 }
